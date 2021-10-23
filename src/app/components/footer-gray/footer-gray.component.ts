46059a96d1e024bcc4128a967e848083edc7e2ce
Bipul04 <73783466+Bipul04@users.noreply.github.com>
2021-10-24 00:17:43 +0545

    Commit made using teleportHQ

---
+++ src/app/components/footer-gray/footer-gray.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'footer-gray',
+  templateUrl: 'footer-gray.component.html',
+  styleUrls: ['footer-gray.component.css'],
+})
+export class FooterGray {
+  rawyt2m: string = ' '
+
+  constructor() {}
+}
